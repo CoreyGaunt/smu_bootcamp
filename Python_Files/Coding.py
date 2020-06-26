61a0de78f5ea03e6789712356c4f9e1892b00541
Corey Gaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-06-25 19:44:16 -0500

    adding smu repo to my github account!

--- Python_Files/Coding.py
+++ Python_Files/Coding.py
@@ -1,27 +1,3 @@
-start = input("Welcome to my function, please enter your name here: ")
-print (" ")
-print ("Hello " + start + ", I hope you are excited to use this feature!")
-print (" ")
-
-def intro():
-  phase1 = input("Pick a number between 1 and 10: ")
-  phase2 = int(phase1)
-  if phase2 <= 3:
-    print (" ")
-    print ("a small picker I see")
-    print (" ")
-    intro()
-  elif phase2 > 3 and phase2 < 6:
-    print (" ")
-    print ("So you're a middle ground monger then..")
-    print (" ")
-    intro()
-  else:
-    print (" ")
-    print ("Only big stacks for us my guy! " + start + ", you are one big dick player!")
-    print (" ")
-    intro()
-    
-intro()
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:7ca9e8588009e8f79886bcada288c86c10c517026224fb6f0af5249f2dfac9ae
+size 645
