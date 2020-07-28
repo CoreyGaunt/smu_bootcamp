5fdce8c36e08d5c22f07060f4a5e3b5535516865
Corey Gaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-07-27 21:24:40 -0500

    uploading my python homework - my .ipynb files work, but my .py aren't! URGH!

---
+++ Working_Class_Folder/Homework/3_Python/PyBank/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b08c36bfc6be26554b8a9afab2677fe78ddcefa52a805631a1d9704aed93578
+size 2548
---
+++ Working_Class_Folder/Homework/3_Python/PyPoll/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2ecdb92276c2cb7628613d32cee19742375ee99c4c38c0c652ce2a0726a59a8d
+size 3129
